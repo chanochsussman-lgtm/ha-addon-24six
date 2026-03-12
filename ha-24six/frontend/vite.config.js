426a3212741f2cf003cd9ec7b3c9e454b0e87015
chanochsussman-lgtm <chanochsussman@gmail.com>
2026-03-12 14:49:25 -0400

    .27

--- ha-24six/frontend/vite.config.js
+++ ha-24six/frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    minify: false,
+    terserOptions: undefined,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8484'
